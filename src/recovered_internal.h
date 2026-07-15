5a4f311d97c7cd326393292a0d6227e860386050
lisu188 <andrzej.lis3@gmail.com>
2026-07-15 13:41:14 +0200

    docs: clarify recovered ABI umbrella

--- src/recovered_internal.h
+++ src/recovered_internal.h
@@ -3,7 +3,7 @@
 #define CLASH95_RECOVERED_INTERNAL_H
 
 /* Shared recovered declaration umbrella. Domain translation units include this
- * until their private declarations can be narrowed without changing the ABI. */
+ * until their private declarations can be narrowed without changing recovered ABI. */
 #include "recovered_functions.h"
 #include "recovered_layout.h"
 
