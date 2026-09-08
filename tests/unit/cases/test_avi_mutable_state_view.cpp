baefe108209b3ce1093d0553eb3fa531f3a61b67
lisu188 <andrzej.lis3@gmail.com>
2026-09-08 08:35:32 +0200

    Test sequential AVI rectangle copy mutators

--- tests/unit/cases/test_avi_mutable_state_view.cpp
+++ tests/unit/cases/test_avi_mutable_state_view.cpp
@@ -21,12 +21,13 @@ TEST(cpp_avi_mutable_state_view, writes_clip_rect_words) {
   using clash95::media::CAviDecompressorView;
 
   std::array<std::byte, CAviDecompressorView::kObjectSize> storage{};
-  CAviDecompressorMutableView(storage.data()).setClipRect(11, 12, 211, 212);
+  const std::uint32_t rect[] = {11u, 12u, 211u, 212u};
+  CAviDecompressorMutableView(storage.data()).setClipRect(rect);
 
-  CHECK_EQ(loadValue<std::int32_t>(storage.data(), CAviDecompressorView::kClipLeftOffset), 11);
-  CHECK_EQ(loadValue<std::int32_t>(storage.data(), CAviDecompressorView::kClipTopOffset), 12);
-  CHECK_EQ(loadValue<std::int32_t>(storage.data(), CAviDecompressorView::kClipRightOffset), 211);
-  CHECK_EQ(loadValue<std::int32_t>(storage.data(), CAviDecompressorView::kClipBottomOffset), 212);
+  CHECK_EQ(loadValue<std::uint32_t>(storage.data(), CAviDecompressorView::kClipLeftOffset), 11u);
+  CHECK_EQ(loadValue<std::uint32_t>(storage.data(), CAviDecompressorView::kClipTopOffset), 12u);
+  CHECK_EQ(loadValue<std::uint32_t>(storage.data(), CAviDecompressorView::kClipRightOffset), 211u);
+  CHECK_EQ(loadValue<std::uint32_t>(storage.data(), CAviDecompressorView::kClipBottomOffset), 212u);
 }
 
 TEST(cpp_avi_mutable_state_view, writes_color_key_contract) {
@@ -46,11 +47,12 @@ TEST(cpp_avi_mutable_state_view, writes_background_rect_and_object) {
   using clash95::media::CAviDecompressorView;
 
   std::array<std::byte, CAviDecompressorView::kObjectSize> storage{};
-  CAviDecompressorMutableView(storage.data()).setBackground(0x10203040, 1, 2, 101, 202);
+  const std::uint32_t rect[] = {1u, 2u, 101u, 202u};
+  CAviDecompressorMutableView(storage.data()).setBackground(0x10203040, rect);
 
   CHECK_EQ(static_cast<std::uint32_t>(loadValue<std::int32_t>(storage.data(), CAviDecompressorView::kBackgroundObjectHandleOffset)), 0x10203040u);
-  CHECK_EQ(loadValue<std::int32_t>(storage.data(), CAviDecompressorView::kBackgroundLeftOffset), 1);
-  CHECK_EQ(loadValue<std::int32_t>(storage.data(), CAviDecompressorView::kBackgroundTopOffset), 2);
-  CHECK_EQ(loadValue<std::int32_t>(storage.data(), CAviDecompressorView::kBackgroundRightOffset), 101);
-  CHECK_EQ(loadValue<std::int32_t>(storage.data(), CAviDecompressorView::kBackgroundBottomOffset), 202);
+  CHECK_EQ(loadValue<std::uint32_t>(storage.data(), CAviDecompressorView::kBackgroundLeftOffset), 1u);
+  CHECK_EQ(loadValue<std::uint32_t>(storage.data(), CAviDecompressorView::kBackgroundTopOffset), 2u);
+  CHECK_EQ(loadValue<std::uint32_t>(storage.data(), CAviDecompressorView::kBackgroundRightOffset), 101u);
+  CHECK_EQ(loadValue<std::uint32_t>(storage.data(), CAviDecompressorView::kBackgroundBottomOffset), 202u);
 }
